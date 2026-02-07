9ec610a2963334eae5f4a5498f40022491e057f9
ao.nekrasov <ao.nekrasov@aisgorod.ru>
2026-02-07 12:08:53 +0300

    'Changes'

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using OOP_lab1;
 
 namespace lab1
 {
@@ -9,54 +10,6 @@ namespace lab1
     //      расстояние (км),
     //      ориентировочное время в пути.
     //  Метод для расчета средней скорости. 
-    class Route
-    {
-        private string _start;
-        private string _end;
-        private double _dist;
-        private double _time;
-
-        public string Start { get { return _start; } }
-        public string End { get { return _end; } }
-        public double Dist { 
-            get { return _dist; } 
-            set { 
-                if (value > 0) 
-                    _dist = value; 
-            } 
-        }
-        public double Time
-        {
-            get { return _time; }
-            set {
-                if (value > 0)
-                    _time = value; 
-            }
-        }
-
-        public Route(string start, string end, double dist, double time)
-        {
-            _start = start;
-            _end = end;
-            _dist = dist;
-            _time = time;
-        }
-
-        public double AverageSpeed()
-        {
-            return _dist / _time;
-        }
-
-        public void TotasTimeCalc(out double total, params double[] stopTimes)
-        {
-            total = 0;
-            foreach (var stop in stopTimes)
-            {
-                total += stop;
-            }
-            total += _time;
-        }
-    }
 
     class Program
     {
@@ -76,7 +29,7 @@ namespace lab1
                               $"Среднее время в пути: {route1.Time}ч (изм)");
 
             // Средняя скорость 
-            Console.WriteLine($"Средняя скорость маршрута: {route1.AverageSpeed}км/ч");
+            Console.WriteLine($"Средняя скорость маршрута: {route1.AverageSpeed()}км/ч");
 
             // Работа с параметрами методов 
             double[] stops = new double[]{ 1, 0.5 };
---
+++ Route.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lab1
+{
+    class Route
+    {
+        private string _start;
+        private string _end;
+        private double _dist;
+        private double _time;
+
+        public string Start { get { return _start; } }
+        public string End { get { return _end; } }
+        public double Dist
+        {
+            get { return _dist; }
+            set
+            {
+                if (value > 0)
+                    _dist = value;
+            }
+        }
+        public double Time
+        {
+            get { return _time; }
+            set
+            {
+                if (value > 0)
+                    _time = value;
+            }
+        }
+
+        public Route(string start, string end, double dist, double time)
+        {
+            _start = start;
+            _end = end;
+            _dist = dist;
+            _time = time;
+        }
+
+        public double AverageSpeed()
+        {
+            return _dist / _time;
+        }
+
+        public void TotasTimeCalc(out double total, params double[] stopTimes)
+        {
+            total = 0;
+            foreach (var stop in stopTimes)
+            {
+                total += stop;
+            }
+            total += _time;
+        }
+    }
+}
